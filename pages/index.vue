a872b3ab7ec07b75c2dac42a380020c9994fb680
matt curschman <mattcurschman@gmail.com>
2023-09-08 11:28:34 -0400

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <MainLayout>
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+        <div v-if="products" v-for="product in products" :key="product">
+          <ProductComponent :product="product"/>
+        </div>
+      </div>
+    </div>
+  </MainLayout>
+
+</template>
+
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
+
+const products = [
+    { id:1, title: "Title", description: "description", url:"https://picsum.photos/id/7/800/800", price:9999},
+  { id:2, title: "Title", description: "description", url:"https://picsum.photos/id/71/800/800", price:9699},
+  { id:3, title: "Title", description: "description", url:"https://picsum.photos/id/72/800/800", price:9599},
+  { id:4, title: "Title", description: "description", url:"https://picsum.photos/id/73/800/800", price:9299},
+  { id:5, title: "Title", description: "description", url:"https://picsum.photos/id/74/800/800", price:93399},
+  { id:6, title: "Title", description: "description", url:"https://picsum.photos/id/75/800/800", price:9919},
+  { id:7, title: "Title", description: "description", url:"https://picsum.photos/id/76/800/800", price:9979},
+]
+</script>
